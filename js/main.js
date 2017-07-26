d581465997d1a2af1ed4e90489b9244770a644c3
Steven Gangano <stevengangano@Stevens-MacBook-Pro.local>
2017-07-25 19:33:03 -0700

    updated pic sizes

--- js/main.js
+++ js/main.js
@@ -39,7 +39,16 @@ $(document).ready(function() {
 });
 
 //slider slideshow
-
 $(document).ready(function (){
 	$(".rslides").responsiveSlides();
 })
+
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  random: true,          // Boolean: Randomize the order of the slides, true or false
+  pause: true,           // Boolean: Pause on hover, true or false
+  pauseControls: true   // Boolean: Pause when hovering controls, true or false 
+});
+
